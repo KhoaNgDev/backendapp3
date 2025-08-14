148b4bcb24463e7d61101199563eb48a3b49f7af
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-14 10:13:36 +0700

    config.cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,24 +1,15 @@
 <?php
 
 return [
-
-    'paths' => ['*', 'sanctum/csrf-cookie'],
-
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'https://app3.webnew.info.vn'
     ],
-
-    'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
+
 
 ];
